53bcc04e141a338370d3a24f336547539a88a508
Augustin Borsu <a.borsu@gmail.com>
2016-10-15 14:27:36 +0200

    chore(setup): add long description

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError, OSError):
+    print('WARN: README conversion to rst skipped.')
+    long_description = open('README.md').read()
+
 setup(
     name='sagaconfig',
     packages=['sagaconfig'],
     package_dir={'sagaconfig': 'lib'},
     version='0.0.1',
     description='A config helper',
+    long_description=long_description,
     author='Sagacify',
     author_email='dev@sagacify.com',
     url='https://github.com/Sagacify/config-py',
