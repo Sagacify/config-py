16043384833a926e59e309cb0261fa84ba862891
Augustin Borsu <a.borsu@gmail.com>
2016-10-17 13:57:02 +0200

    version(0.0.2): fix setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='sagaconfig',
     packages=['sagaconfig'],
     package_dir={'sagaconfig': 'src'},
-    version='0.0.1',
+    version='0.0.2',
     description='A config helper',
     long_description=long_description,
     author='Sagacify',
